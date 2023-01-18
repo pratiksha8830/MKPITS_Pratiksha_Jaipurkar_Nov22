1fc1ef2373a431e409a691ec38dec1fea1c0f27a
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-18 22:53:34 +0530

    18 jan arraylength

---
+++ arraydemolength.java
@@ -0,0 +1,21 @@
+class one{
+void fun(){
+int a[]={1,2,3,4};
+int []a1;
+
+for(int i=0;i<=3;i++)
+{
+System.out.println(a[i]);
+}
+for(int i=0;i<=a.length; i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+public class arraydemolength{
+ public static void main(String[]args){
+ one ob = new one();
+ ob.fun();
+ }
+ }
