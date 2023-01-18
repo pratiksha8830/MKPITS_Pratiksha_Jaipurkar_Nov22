9e3f42cde0493cf0df0a8d8b363e268504eab557
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-18 23:11:19 +0530

    17 jan triangle java

---
+++ triangle.java
@@ -0,0 +1,21 @@
+class tri
+{
+int i,sum=0;
+void set()
+{
+for(i=1;i<30;i++)
+{
+    sum=sum+i;
+  System.out.println(sum);
+  }
+
+}
+}
+class triangle
+{
+public static void main(String[]args)
+{
+tri ob=new tri();
+ob.set();
+}
+}
