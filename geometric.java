a1a6373bb5cac5a0cd414911a08cfc746d9c0321
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-18 17:36:06 +0530

    17 jan assign

---
+++ geometric.java
@@ -0,0 +1,22 @@
+class geo
+{
+int i;
+
+void set()
+
+{
+for(i=5;i<=300;i=i*2)
+{
+System.out.println(i);
+}
+}
+}
+class  geometric
+{
+public static void main (String[]args)
+{
+geo ob1= new geo();
+ob1.set();
+}
+}
+
