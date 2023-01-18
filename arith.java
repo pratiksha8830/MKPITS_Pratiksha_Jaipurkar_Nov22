0ed0d74fb872f3ca88b70dbc9b4e57ed07b7e4eb
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-18 23:07:59 +0530

    17 jan arith java

---
+++ arith.java
@@ -0,0 +1,21 @@
+class num
+{
+int i;
+void set()
+{
+for(i=1;i<30;i=i+3)
+{
+
+  System.out.println(i);
+  }
+
+}
+}
+class arith
+{
+public static void main(String[]args)
+{
+num ob1=new num();
+ob1.set();
+}
+}
