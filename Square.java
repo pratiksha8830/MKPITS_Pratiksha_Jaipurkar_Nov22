12ceacb32e7f40ec88d295c71746fa00ce97bb36
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-18 23:03:44 +0530

    17 jan square java

---
+++ Square.java
@@ -0,0 +1,21 @@
+class squ
+{
+int i,sum=0;
+void set()
+{
+for(i=1;i<10;i++)
+{
+  sum=i*i;
+  System.out.println(sum);
+  }
+
+}
+}
+class Square
+{
+public static void main(String[]args)
+{
+squ ob=new squ();
+ob.set();
+}
+}
