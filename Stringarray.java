f94ac7829f1a8efb7f5a6a5e2cc39f31cc351c82
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-24 14:44:42 +0530

    24 jan Stringarray java

---
+++ Stringarray.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Stringarray
+{
+public static void main(String[]args)
+{
+char data[]={'a','b','c',};
+String sob=new String(data);
+System.out.println(sob);
+}
+}
