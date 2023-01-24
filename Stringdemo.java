32e89dd1a7e95a6f5b01bc463325803bb6378bba
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-24 14:39:20 +0530

    24 jan string demo  java

---
+++ Stringdemo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Stringdemo
+{
+public static void main(String[]args){
+String s="pratiksha";
+char ch;
+for(int i=0; i<9; i++){
+ch=s.charAt(i);
+System.out.println(ch);
+}
+}
+}
