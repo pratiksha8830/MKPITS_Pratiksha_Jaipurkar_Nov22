61261c95bab0d1a4ac9f0f1b1b71aee514798494
pratiksha8830 <121150787+pratiksha8830@users.noreply.github.com>
2023-01-24 14:50:49 +0530

    24 jan Stringlength java

---
+++ Stringlength.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Stringlength
+{
+public static void main(String[]args)
+{
+char data[]={'a','b','c','d','e','f','g'};
+String sob=new String(data);
+System.out.println(sob);
+
+{
+int len=sob.length();
+System.out.println(len);
+}
+}
+}
