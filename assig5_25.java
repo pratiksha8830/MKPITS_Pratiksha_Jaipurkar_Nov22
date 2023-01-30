d836ee8d286f6c92319a81d5699500df9a1d33ed
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-01-30 18:06:27 +0530

    assign 5 25 jan

---
+++ assig5_25.java
@@ -0,0 +1,17 @@
+class assig5_25
+{
+public static void main(String[]args)
+{
+int [] arr = {2,5,4,6,8,9,3 };
+int [] arrNew = {arr[3],arr[4],arr[5],arr[6]};
+System.out.print("arrNew []: ");
+
+
+for(int i=0; i<arrNew.length;i++)
+{
+	System.out.print(arrNew[i]+" ");
+}
+
+}
+
+}
