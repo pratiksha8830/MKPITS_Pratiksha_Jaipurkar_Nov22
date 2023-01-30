8e32715eab77bed6704657bd5cee0f8c2c354dc4
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-01-30 18:18:24 +0530

    assign14 25 jan

---
+++ assig14_25.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class assig14_25
+{
+public static void main(String[]args)
+{
+	System.out.println("Enter the number");
+Scanner sc=new Scanner(System.in);
+
+int num=sc.nextInt();
+if(num<=3)
+{
+	System.out.println("the number is greater " +num);
+}
+else
+{
+	System.out.println("the number is not greater" +num);
+}
+}
+}
