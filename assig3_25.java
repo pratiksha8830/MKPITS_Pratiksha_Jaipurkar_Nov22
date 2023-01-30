aebe7b4f29b75e8884fd35e75cb7ab8dfc55a24e
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-01-30 15:24:19 +0530

    assign3 25 jan

---
+++ assig3_25.java
@@ -0,0 +1,16 @@
+class assig3_25
+{
+public static void main(String[]args)
+{
+int a[]={1,2,3 };
+int b[]={4,5,6};
+
+int c[]={a[0],a[1],a[2],b[0],b[1],b[2]};
+for(int i=0; i<c.length ;i++)
+{
+	System.out.print(" "+c[i]);
+}
+
+}
+
+}
