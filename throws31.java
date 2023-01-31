b784242c00cfd194f380f8d28475c32f2bde7f3e
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-01-31 16:03:10 +0530

    throws31 jan

---
+++ throws31.java
@@ -0,0 +1,9 @@
+class throws31
+{
+public static void main(String[]args)throws InterruptedException
+{
+System.out.println("start program");
+Thread.sleep(100);
+System.out.println("end program");
+}
+}
