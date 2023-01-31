5a3a36b04f1eb21f611ed51fc8746ad2f8e44a8f
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-01-31 16:56:03 +0530

    simple throw jan 31

---
+++ thro.java
@@ -0,0 +1,30 @@
+class thro
+{
+
+public static void main(String[]args)
+{
+int bal = 3000;
+int width = 5000;
+if (bal>width)
+{
+System.out.println("rupay le lo");
+}
+else
+{
+ArithmeticException ob = new ArithmeticException();
+throw ob;
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
