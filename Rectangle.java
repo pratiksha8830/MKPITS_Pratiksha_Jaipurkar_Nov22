1a8c3c8bdd55d84016f31023d0661be1a2ecff5f
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-02-22 17:20:16 +0530

    circle

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+
+class Rectangle
+{
+public static  void main(String[]args)
+{
+
+int height=7;
+int width=5;
+double area=height*width;
+double perimeter=2*(height+width);
+System.out.println(area);
+System.out.println(perimeter);
+
+}
+}
