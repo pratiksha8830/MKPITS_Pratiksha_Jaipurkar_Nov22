ede4c918ef4e9858f8f32080b7bee570e90ae367
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-02-22 17:26:43 +0530

    area_of_circle

---
+++ area_of_circle.java
@@ -0,0 +1,11 @@
+//write a java program to compute  the paremeter and area of circle with radius of 6 inch//
+
+class area_of_circle
+{
+	public static void main(String[]args){
+
+		int radius=6;
+		double area=3.14*radius*radius;
+		System.out.println(area);
+	}
+}
