3130ace08f21a82213c7fe683323a076813b656a
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-02-27 22:49:12 +0530

    assig6_feb1

---
+++ assig6_feb1.java
@@ -0,0 +1,22 @@
+
+//*
+
+import java.util.Scanner;
+class assig6_feb1
+{
+public static void main(String []args)
+{
+Scanner input = new
+Scanner(System.in);
+
+System.out.print("Enter a number:");
+int num = input.nextInt();
+
+for(int i=0;i<5; i++){
+for(int j=0;j<3; j++){
+	System.out.print(num);
+}
+ System.out.println();
+ }
+ }
+ }
