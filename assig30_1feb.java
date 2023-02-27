a509b24a9e30c717b5e36d074b24060bf3310094
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-02-27 14:06:04 +0530

    assig30_feb1

---
+++ assig30_1feb.java
@@ -0,0 +1,14 @@
+class assig30_1feb
+{
+
+public static void main(String[]args)
+{
+
+System.out.println("The first 10 natural numbers are : ");
+for (int i=0;i<=11; i++)
+{
+
+  System.out.println(i);
+}
+}
+}
