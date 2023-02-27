983738f5690390224b9b053c4a2cb354b309484b
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-02-27 14:04:13 +0530

    assig17_feb1

---
+++ assig17_feb1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+  
+  class assig17_feb1
+  {
+  private  static Scanner sc;
+  
+  public static void main(String[] args){
+  
+  double side1,side2,side3;
+  sc=new Scanner(System.in);
+  
+  System.out.println("please Enter Three sides of Triangle");
+  side1 = sc.nextDouble();
+  side2 = sc.nextDouble();
+  side3 = sc.nextDouble();
+  
+  
+  if(side1==side2 && side2==side3)
+  {
+  System.out.println("It is an Equilateral Triangle");
+  }
+  
+  else if (side1==side2 ||side2==side3||side1==side3)
+  
+  {
+     System.out.println("It is an Isosceles Triangle");
+     
+     }
+     else
+     {
+     
+       System.out.println("It is a Scalene Triangle");
+       }
+       }
+       }
