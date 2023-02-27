283152a6bcb105000e820966b0339aaafc1bfe74
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-02-27 14:02:54 +0530

    assig5_feb1

---
+++ assig5_feb1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class assig5_feb1
+{
+public static void main(String[]args)
+{
+	Scanner input= new
+Scanner(System.in);
+System.out.println("Enter your age:");
+int age=input.nextInt();
+
+System.out.println("you look older than"+age);
+}
+}
