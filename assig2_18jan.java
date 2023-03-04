4d3ab081826ca35ad5f9a533e7c2cfa12f4b6094
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-04 14:30:30 +0530

    assig2_jan18

---
+++ assig2_18jan.java
@@ -0,0 +1,15 @@
+//*write a java program to shift given data by two bits to the left*//
+
+import java.util.Scanner;
+class assig2_18jan
+{
+public static void main(String[]args)
+{
+int  data = 10;  //data to be shifted
+int shiftedData = data << 2;  //shift left by 2 bits
+System.out.println("Original data:" + data);
+System.out.println("Shifted data:" + shiftedData);
+
+}
+}
+
