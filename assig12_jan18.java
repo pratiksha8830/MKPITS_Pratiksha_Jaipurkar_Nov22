e562e588d39c1e605c092df0e44da56b46223687
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-04 14:59:12 +0530

    assig12_jan18

---
+++ assig12_jan18.java
@@ -0,0 +1,13 @@
+
+
+//*write a java program  to print the alphabet set in decimal and character form*//
+
+import java.util.Scanner;
+class assig12_jan18{
+public static void main(String[]args){
+
+for(int i=65; i<=90; i++){
+System.out.printf("%d: %c\n",i,(char)i);
+}
+}
+}
