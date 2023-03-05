561af5b5789341ff6a5664ea12eeef4543bbba65
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-05 23:37:05 +0530

    Basic_17

---
+++ Basic_17.java
@@ -0,0 +1,27 @@
+//*write a program  to display equilateral triangle with stars*//
+
+import java.util.Scanner;
+
+class Basic_17{
+public static void main(String[]args){
+
+Scanner sc=new 
+Scanner(System.in);
+
+System.out.println("enter the number");
+int n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+ for(int j=0;j<n-i-1;j++)
+ {
+ System.out.print(" ");
+ }
+ for(int k=0;k<=i;k++)
+ {
+ System.out.print(" *");
+ 
+ }
+ System.out.println();
+ }
+ }
+ }
