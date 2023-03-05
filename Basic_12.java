e910dd6eaf10e18e941f7921a4bba63cf47971e5
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-05 23:16:55 +0530

    Basic_12

---
+++ Basic_12.java
@@ -0,0 +1,20 @@
+//*write a program to reverse the number*//
+
+import java.util.Scanner;
+class Basic_12{
+
+public static void main(String[]args){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+int num=sc.nextInt();
+int t=num;
+int rev=0;
+while(num!=0)
+{
+  rev=rev*10+(num%10);
+  num=num/10;
+  }
+  System.out.println(rev);
+  }
+  }
