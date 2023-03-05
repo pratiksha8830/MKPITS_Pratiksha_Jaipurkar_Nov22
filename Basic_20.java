d879af61720d7e834d991c3017234fec3ed58022
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-05 23:51:23 +0530

    Basic_20

---
+++ Basic_20.java
@@ -0,0 +1,28 @@
+//*write a program to display the hallow box with stars*//
+
+class Basic_20{
+
+public static void main(String[]args){
+
+java.util.Scanner sc= new 
+java.util.Scanner(System.in);
+System.out.println("enter value of n");
+
+int n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+for (int j=0;j<n;j++)
+{
+if(i==0||j==0||i==n-1||j==n-1)
+{
+System.out.print("*");
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}
