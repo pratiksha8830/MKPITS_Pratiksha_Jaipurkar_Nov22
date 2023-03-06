77729682eab4f98717ca3243062677a01e1d65e5
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-06 12:46:14 +0530

    Basic_28

---
+++ Basic_28.java
@@ -0,0 +1,30 @@
+//*write a program to display M pattern with stars*//
+
+class Basic_28{
+
+public static void main(String[]args){
+
+int spaces=8;
+for (int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+
+for(int k=1;k<=spaces;k++)
+{
+
+System.out.print(" ");
+}
+
+for(int I=1;I<=i;I++)
+{
+System.out.print("*");
+
+}
+System.out.println();
+spaces-=2;
+}
+}
+}
