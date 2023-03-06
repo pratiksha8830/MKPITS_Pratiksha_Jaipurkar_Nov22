97e3e6b74096dfb368a696067f21d7b7ca37fb0d
pratiksha 8830 <jaipurkarpratiksha@gmail.com>
2023-03-06 12:53:23 +0530

    Basic_29

---
+++ Basic_29.java
@@ -0,0 +1,25 @@
+//*write a program to display sequence of numbers in triangle format*//
+
+import java.util.Scanner;
+class Basic_29{
+
+public static void main(String[]args){
+
+Scanner sc=new
+Scanner(System.in);
+System.out.println("enter the rows");
+int n=sc.nextInt();
+int k=0;
+for (int i=1;i<=n;i++)
+{
+for (int j=1;j<=1; j++)
+
+{
+
+k++;
+System.out.print(k+"");
+}
+System.out.println("");
+}
+}
+}
